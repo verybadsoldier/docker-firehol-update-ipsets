34dff7d4723806368837f3093a26ab2b22348d77
verybadsoldier <vbs@springrts.de>
2019-02-18 23:18:30 +0100

    several changes... * delete .source when calling disable * do not block all ipsets in iptables but just the ones used by FireHOL * do not auto-enable sub-lists (cannot deny single lists and disable not implemented)

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --name firehol-update-ipsets -it -d --restart=always --cap-add=NET_ADMIN --net=host devrt/firehol-update-ipsets
+docker run --name firehol-update-ipsets -it -d --restart=always --cap-add=NET_ADMIN --net=host verybadsoldier/firehol-update-ipsets
