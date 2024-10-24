adf43e06ed001042cd8d24e9469183ceff019ba3
verybadsoldier <vbs@springrts.de>
2024-10-24 22:04:15 +0200

    some refactors

--- update-common.sh
+++ update-common.sh
@@ -14,3 +14,10 @@ function should_skip {
   done
   return 0
 }
+
+
+function add_rule_for_set {
+  if [[ -z `${IPTABLES_CMD} -L FIREHOL_BLACKLIST | grep "match-set $1 src"` ]]; then
+    ${IPTABLES_CMD} -I FIREHOL_BLACKLIST -m set --match-set $1 src -j DROP -m comment --comment "FireHOL: $1"
+  fi
+}
